5c4a79eced500462681b397ef5ad066937aa4ed4
MinionWave37 <Minionwave100@gmail.com>
2023-07-04 14:38:59 -0500

    ArrowCode

--- Assets/Enemies/Base/EProjectile.cs
+++ Assets/Enemies/Base/EProjectile.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 
 public class EProjectile : MonoBehaviour
 {
-    
     public float damage;
     public float Speed;
     public float LifeTime;
+    public Vector3 direction;
+    public bool shootTowardsPlayer;
+    public bool followPlayer;
+
+    private Transform playerTransform;
+    private Vector3 playerDirection;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        playerDirection = (playerTransform.position - transform.position).normalized;
+    }
 
     void Update()
     {
-        transform.position += Vector3.down * Speed * Time.deltaTime;
-        LifeTime -= 1 * Time.deltaTime;
+        if (shootTowardsPlayer)
+        {            
+            transform.position += playerDirection * Speed * Time.deltaTime;
+        }
+        else if (followPlayer)
+        {
+            transform.position += (playerTransform.position - transform.position).normalized * Speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += direction * Speed * Time.deltaTime;
+        }
+
+        LifeTime -= Time.deltaTime;
+        if (LifeTime < 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "CharaIcon")
         {
-            GameObject.FindGameObjectWithTag("Player").transform.gameObject.SendMessage("TakeDam", damage * 3);
+            GameObject.FindGameObjectWithTag("Player").SendMessage("TakeDam", damage * 3);
             Destroy(gameObject);
         }
     }
-
 }
